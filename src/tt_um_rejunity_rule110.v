014aa54fd052ac01fe9d37c31e6031de7ad41430
Renaldas Zioma <rej@unity3d.com>
2023-09-10 22:28:36 +0200

    rule110 module

--- src/tt_um_rejunity_rule110.v
+++ src/tt_um_rejunity_rule110.v
@@ -1,5 +1,20 @@
 `default_nettype none
 
+module rule110 (
+    input  wire [2:0] in,
+    output reg out
+);
+    always @(*) begin
+        case(in)
+            3'b000:  out = 1'b0; // ...
+            3'b100:  out = 1'b0; // X..
+            3'b111:  out = 1'b0; // XXX
+            default:    
+                out = 1'b1;
+        endcase
+    end
+endmodule
+
 module tt_um_rejunity_rule110 #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
@@ -20,7 +35,9 @@ module tt_um_rejunity_rule110 #( parameter MAX_COUNT = 24'd10_000_000 ) (
         end
     end
 
+    wire out_;
+    assign uo_out[0] = out_;
     // instantiate segment display
-    //seg7 seg7(.counter(digit), .segments(led_out));
+    rule110 rule110(.in(ui_in[2:0]), .out(out_));
 
 endmodule
