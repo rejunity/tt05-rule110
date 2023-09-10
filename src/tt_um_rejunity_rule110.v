d530cb9eb3f2205398c15952432648db0102bad7
Renaldas Zioma <rej@unity3d.com>
2023-09-10 16:00:51 +0200

    Made changes similar to tt04-verilog-demo

---
+++ src/tt_um_rejunity_rule110.v
@@ -0,0 +1,26 @@
+`default_nettype none
+
+module tt_um_rejunity_rule110 #( parameter MAX_COUNT = 24'd10_000_000 ) (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+    wire reset = ! rst_n;
+
+    always @(posedge clk) begin
+        // if reset, set counter to 0
+        if (reset) begin
+        end else begin
+        end
+    end
+
+    // instantiate segment display
+    //seg7 seg7(.counter(digit), .segments(led_out));
+
+endmodule
