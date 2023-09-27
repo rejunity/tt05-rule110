60361888352626182cfd0fce66ca1166af921ab0
rej <rzioma@acm.org>
2023-09-27 13:11:34 +0200

    Silently ignore lack of access to internal state when logging in GL_test

--- src/test.py
+++ src/test.py
@@ -2,6 +2,20 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+def print_automaton_state(dut):
+    try:
+        internal = dut.tt_um_rejunity_rule110_uut
+        print(
+            str(internal.cells.value).replace('0', ' ').replace('1', '@'),
+            "in:",   dut.ui_in.value,
+            "ctrl:", dut.uio_in.value,
+            "out:",  dut.uo_out.value)
+    except: # No access to internal state during the Gate Level simulation,
+            # so we are just going to silently ignore exception and
+            # print only outputs pins instead
+        print(dut.uo_out.value)
+
+
 # @cocotb.test()
 async def test_rule110(dut):
     RULE110_TRUTH_TABLE = [0,1,1,1, 0,1,1,0]
@@ -25,8 +39,6 @@ async def test_rule110(dut):
 
     dut._log.info("done")
 
-
-
 @cocotb.test()
 async def test_rule110_automata(dut):
 
@@ -37,8 +49,8 @@ async def test_rule110_automata(dut):
     dut._log.info("reset")
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
-    print(str(dut.tt_um_rejunity_rule110_uut.cells.value).replace('0', ' ').replace('1', '@'))
-    print("",str(dut.tt_um_rejunity_rule110_uut.cells_dt.value).replace('0', ' ').replace('1', '@'))
+    # print(str(dut.tt_um_rejunity_rule110_uut.cells.value).replace('0', ' ').replace('1', '@'))
+    # print("",str(dut.tt_um_rejunity_rule110_uut.cells_dt.value).replace('0', ' ').replace('1', '@'))
     dut.rst_n.value = 1
     dut.uio_in.value = -1
 
@@ -65,13 +77,7 @@ async def test_rule110_automata(dut):
     for i in range(128+32):
     # for i in range(32):
         await ClockCycles(dut.clk, 1)
-        print(
-            # dut.tt_um_rejunity_rule110_uut.write_enable.value, dut.tt_um_rejunity_rule110_uut.halt.value,
-            #str(dut.tt_um_rejunity_rule110_uut.cells_dt.value).replace('0', ' ').replace('1', '@'),
-            str(dut.tt_um_rejunity_rule110_uut.cells.value).replace('0', ' ').replace('1', '@'),
-            dut.ui_in.value,
-            dut.uio_in.value,
-            dut.uo_out.value)
+        print_automaton_state(dut)
 
     dut._log.info("done")
 
